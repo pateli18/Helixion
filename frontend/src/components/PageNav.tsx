b17642b0bd22af198b88172d0c0281ac46fff7c8
Ihsaan Patel <ihsaan.patel@gmail.com>
2025-02-03 21:28:12 -0800

    Display organization name

--- frontend/src/components/PageNav.tsx
+++ frontend/src/components/PageNav.tsx
@@ -17,6 +17,8 @@ import { PersonIcon } from "@radix-ui/react-icons";
 import { ArchiveIcon } from "lucide-react";
 import { useLocation } from "react-router-dom";
 import { UserNav } from "./UserNav";
+import { useAuthInfo } from "@propelauth/react";
+import { UseAuthInfoProps } from "@propelauth/react/dist/types/hooks/useAuthInfo";
 
 const SidebarItem = (props: {
   icon: React.ReactNode;
@@ -42,8 +44,19 @@ const SidebarItem = (props: {
   );
 };
 
+const getOrganizationName = (authInfo: UseAuthInfoProps) => {
+  const orgs = authInfo.orgHelper?.getOrgs();
+  if (orgs?.length === 1) {
+    return orgs[0].orgName;
+  }
+  return undefined;
+};
+
 export const PageSidebar = () => {
+  const authInfo = useAuthInfo();
   const { isMobile, state } = useSidebar();
+
+  const organizationName = getOrganizationName(authInfo);
   return (
     <Sidebar collapsible="icon">
       <SidebarHeader>
@@ -51,9 +64,16 @@ export const PageSidebar = () => {
           <SidebarMenuItem>
             <div className="flex items-center gap-2">
               <img src="/logo.png" alt="Logo" className="h-auto w-10" />
-              {!isMobile && state !== "collapsed" && (
-                <span className="text-lg font-semibold">Helixion</span>
-              )}
+              <div className="flex flex-col">
+                {!isMobile && state !== "collapsed" && (
+                  <span className="text-lg font-semibold">Helixion</span>
+                )}
+                {!isMobile && state !== "collapsed" && organizationName && (
+                  <span className="text-sm text-muted-foreground">
+                    {organizationName}
+                  </span>
+                )}
+              </div>
             </div>
           </SidebarMenuItem>
         </SidebarMenu>
